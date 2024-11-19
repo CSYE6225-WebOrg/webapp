3c2692925c8a93246cea2684a4d8826d671f2792
Yugal Lakhyani <lakhyani.y@northeastern.edu>
2024-11-19 12:40:01 -0500

    changes for a08

--- services/s3Service.js
+++ services/s3Service.js
@@ -2,8 +2,6 @@ import AWS from 'aws-sdk';
 
 
 const s3 = new AWS.S3({
-  accessKeyId : process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRETS_KEY,
   region: process.env.AWS_REGION
 });
 
